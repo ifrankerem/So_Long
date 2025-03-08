a1926f7ace0d5e91bf84672c44716dd75f5398c7
ifrankerem <irfankeremarslann@icloud.com>
2025-03-09 01:45:32 +0300

    Converted submodules to regular files

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iarslan <iarslan@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/31 20:28:46 by iarslan           #+#    #+#             */
+/*   Updated: 2025/03/08 01:33:03 by iarslan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+void	*gnl_calloc(size_t count, size_t size);
+char	*gnl_strchr(const char *s, int c);
+char	*gnl_strjoin(char const *s1, char const *s2);
+char	*gnl_strdup(const char *s1);
+void	*gnl_memcpy(void *dest, const void *src, size_t n);
+
+#endif
